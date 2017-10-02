fd5d2b193a5effc90d0fb6edad7491b83879ec36
Michael Lo <ilivehk@michaeldemacbook-air.local>
2017-10-02 11:27:31 -0700

    Initial Commit

---
+++ Father Leung/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Father Leung
+//
+//  Created by Michael Lo on 2/10/2017.
+//  Copyright © 2017年 noading. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
